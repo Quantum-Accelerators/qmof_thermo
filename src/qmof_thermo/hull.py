5ff8bfff6671abf38ca0291a44d585952dc03375
Andrew S. Rosen <asrosen93@gmail.com>
2026-07-01 18:53:16 -0400

    Enhance energy type handling and logging in hull.py

--- src/qmof_thermo/hull.py
+++ src/qmof_thermo/hull.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 from logging import getLogger
 from pathlib import Path
-from typing import TYPE_CHECKING, TypedDict
+from typing import Literal, TYPE_CHECKING, TypedDict
 
 from ase import Atoms
 from monty.serialization import loadfn
@@ -35,7 +35,9 @@ class HullOutput(TypedDict):
 
 def get_energy_above_hull(
     struct: Structure | Atoms,
+    *,
     energy: float,
+    energy_type: Literal["DFT", "ODAC_MLIP"],
     serialized_phase_diagram: Path | str = _DEFAULT_PD_JSON,
 ) -> HullOutput:
     """
@@ -48,6 +50,8 @@ def get_energy_above_hull(
         If an Atoms object is provided, it will be converted to a Structure.
     energy
         Total relaxed energy of the structure in eV.
+    energy_type
+        How the energy data was sourced, either from "DFT" or "ODAC_MLIP"
     serialized_phase_diagram
         Path to the directory containing the precomputed PatchedPhaseDiagram.
         Default is the phase diagram bundled with the package.
@@ -58,26 +62,28 @@ def get_energy_above_hull(
         A dictionary containing energy above the convex hull, formation energy, and decomposition products.
     """
 
+    energy_type = energy_type.lower()
     if isinstance(struct, Atoms):
         struct = AseAtomsAdaptor.get_structure(struct)
 
     mol_elements = {str(el) for el in struct.composition.elements}
-    incompatible = mol_elements - QMOF_COMPATIBLE_ELEMENTS
-
-    if incompatible:
-        LOGGER.warning(
-            "Structure contains elements whose ODAC "
-            f"pseudopotentials do not match QMOF's: {sorted(incompatible)}. "
-            "If `energy` is obtained from an ODAC MLIP, the results may not be comparable to the QMOF DFT reference "
-            "data. Please refer to https://github.com/Quantum-Accelerators/qmof_thermo#faq for more information."
-        )
 
     out_of_chemical_space = mol_elements - QMOF_ELEMENTS
     if out_of_chemical_space:
         LOGGER.warning(
-            "Structure contains elements that are not present in the QMOF chemical space: "
-            f"{sorted(out_of_chemical_space)}. Results may not be comparable to QMOF convex hull phase diagram."
+            "Your structure contains elements that are not present in the QMOF chemical space: "
+            f"{sorted(out_of_chemical_space)}. The convex hull phase diagram will be incomplete."
         )
+
+    
+    if energy_type == "odac_mlip":
+        if incompatible := mol_elements - QMOF_COMPATIBLE_ELEMENTS
+            LOGGER.warning(
+                "Structure contains elements whose ODAC "
+                f"pseudopotentials do not match QMOF's: {sorted(incompatible)}. "
+                "If `energy` is obtained from an ODAC MLIP, the results may not be comparable to the QMOF DFT reference "
+                "data. Please refer to https://github.com/Quantum-Accelerators/qmof_thermo#faq for more information."
+            )
 
     ppd = loadfn(serialized_phase_diagram)
 
